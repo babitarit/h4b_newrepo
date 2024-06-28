0b50848a5980bf58da7cabc822769123cacc6b46
Puskar-Roy <puskarroy600@gmail.com>
2024-06-29 02:03:46 +0530

    feat : change page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,34 @@
+"use client";
+
+import React, { useEffect } from "react";
+import Contact from "../components/Contact";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Vision from "../components/Vision";
 
 export default function Home() {
+  useEffect(() => {
+
+    document.documentElement.style.scrollBehavior = "smooth";
+  }, []);
+
   return (
-    <div className=''>
-     <p className="text-indigo-500">h4b</p>
+    <div className="flex flex-col min-h-screen w-full">
+      <Navbar />
+      <Hero />
+
+      <section id="vision">
+        <Vision />
+      </section>
+      <section id="features">
+        <Features />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
     </div>
   );
 }
