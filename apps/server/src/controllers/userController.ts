61cf8715368a3d1b140bec9daa6d5aa200b36ef8
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-06-29 14:57:02 +0530

    show a user

--- apps/server/src/controllers/userController.ts
+++ apps/server/src/controllers/userController.ts
@@ -23,8 +23,29 @@ export const updateUser = async (req: Request, res: Response) => {
 };
 
 
-export const deleteUser = async (req: Request, res: Response) => {
+export const showUser = async (req: Request, res: Response) => {
     const userId = req.params.id;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error'
+        });
+    }
+};
+export const deleteUser = async (req: Request, res: Response) => {
+    const userId = req?.params.id;
     try {
         await prisma.user.delete({
             where: {
