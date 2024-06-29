18b6d935121a42c6f35c23e360588beb7a9ad305
sakshi <ayushmanj138@gmail.com>
2024-06-29 17:02:13 +0530

    fix : faeature

--- apps/web/components/Features.tsx
+++ apps/web/components/Features.tsx
@@ -2,18 +2,18 @@ import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/2
 
 const features = [
     {
-        name: 'Push to deploy.',
+        name: 'Text to speech',
         description:
             'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
         icon: CloudArrowUpIcon,
     },
     {
-        name: 'SSL certificates.',
+        name: 'Sign chart',
         description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
         icon: LockClosedIcon,
     },
     {
-        name: 'Database backups.',
+        name: 'Learning Module',
         description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
         icon: ServerIcon,
     },
@@ -26,8 +26,8 @@ export default function Features() {
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div className="lg:pr-8 lg:pt-4">
                         <div className="lg:max-w-lg">
-                            <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-                            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+                            {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2> */}
+                            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Accessibility Features</p>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                                 iste dolor cupiditate blanditiis ratione.
