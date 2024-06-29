1890e15cbc82d0e13ac247ddb3afb90916c3ad8a
Puskar-Roy <puskarroy600@gmail.com>
2024-06-29 09:15:53 +0530

    feat : login

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import log_img from '../../../public/Login.png'
-import googlelogo from '../../../public/Google logo.png'
+import log_img from "../../../public/Login.png";
+import googlelogo from "../../../public/Google logo.png";
 import Image from "next/image";
+import { useLogin } from "../../../hooks/useLogin";
 
 import { Alata } from "next/font/google";
 
@@ -14,7 +15,7 @@ export const alata = Alata({
 });
 
 export default function Page(): JSX.Element {
-
+  const { login, error, isLoading, isSucess } = useLogin();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
@@ -31,67 +32,70 @@ export default function Page(): JSX.Element {
       password,
     };
 
-    console.log(loginData);
-    
+    await login(loginData);
   };
   return (
-    <main className='w-full h-screen flex'>
-      <div className='grid grid-cols-1 md:grid-cols-2 m-auto h-[450px] sm:max-w-[800px] rounded-2xl'>
-        <div className='w-full h-[400px] hidden md:block'>
+    <main className="w-full h-screen flex">
+      <div className="grid grid-cols-1 md:grid-cols-2 m-auto h-[450px] sm:max-w-[800px] rounded-2xl">
+        <div className="w-full h-[400px] hidden md:block">
           <Image src={log_img} alt="efef" />
         </div>
-        <div className='p-4 flex flex-col justify-around'>
+        <div className="p-4 flex flex-col justify-around">
           <form>
-            <h2 className='text-4xl font-medium text-center mb-8'>
-              Access Your <span className='text-violet-700'>Account</span>
+            <h2 className="text-4xl font-medium text-center mb-8">
+              Access Your <span className="text-violet-700">Account</span>
             </h2>
             <div>
               <input
-                className='border p-2 w-full mb-4 rounded-xl bg-gray-300'
-                type='text'
-                placeholder='Email'
+                className="border p-2 w-full mb-4 rounded-xl bg-gray-300"
+                type="text"
+                placeholder="Email"
                 value={email}
                 onChange={handleEmailChange}
               />
             </div>
             <div>
               <input
-                className='border p-2 w-full rounded-xl bg-gray-300'
-                type='password'
-                placeholder='Password'
+                className="border p-2 w-full rounded-xl bg-gray-300"
+                type="password"
+                placeholder="Password"
                 value={password}
                 onChange={handlePasswordChange}
               />
             </div>
-            <div className='mt-6 text-right'>
-              <button className='font-medium text-base text-violet-700'><Link href="/forgot-password">
-                Forgot Password?</Link>
+            <div className="mt-6 text-right">
+              <button className="font-medium text-base text-violet-700">
+                <Link href="/forgot-password">Forgot Password?</Link>
               </button>
             </div>
-            <button 
-            className='w-full mt-4 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all p-2 rounded-xl bg-violet-700 text-white text-lg font-bold'
-            key="1"
-            onClick={handleClick}
-            >Login
-            </button>
-            <h5 className=" text-center mb-2 mt-2" >or</h5>
-            <div className="flex items-center justify-center ">
-            <button className=" w-full flex rounded-2xl py-2 border-violet-700 border-2 items-center justify-center gap-2 active:scale-[.98] active:duration-75  hover:scale-[1.01] ease-in-out transition-all"
+            <button
+              className="w-full mt-4 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all p-2 rounded-xl bg-violet-700 text-white text-lg font-bold"
+              key="1"
+              onClick={handleClick}
             >
-              <Image className="w-6 h-6" src={googlelogo} loading="lazy" alt="google logo" />
-               <span>Continue with Google</span>
-           </button>
-          </div>
-            <div className='mt-6 flex justify-center items-center'>
-              <p className='font-medium text-base'>Did not have an Account?</p>
-              <button className='text-violet-700 text-base font-medium ml-2'><Link href="/register">
-                Register</Link>
+              Login
+            </button>
+            <h5 className=" text-center mb-2 mt-2">or</h5>
+            <div className="flex items-center justify-center ">
+              <button className=" w-full flex rounded-2xl py-2 border-violet-700 border-2 items-center justify-center gap-2 active:scale-[.98] active:duration-75  hover:scale-[1.01] ease-in-out transition-all">
+                <Image
+                  className="w-6 h-6"
+                  src={googlelogo}
+                  loading="lazy"
+                  alt="google logo"
+                />
+                <span>Continue with Google</span>
+              </button>
+            </div>
+            <div className="mt-6 flex justify-center items-center">
+              <p className="font-medium text-base">Did not have an Account?</p>
+              <button className="text-violet-700 text-base font-medium ml-2">
+                <Link href="/register">Register</Link>
               </button>
             </div>
           </form>
         </div>
       </div>
     </main>
-
   );
 }
