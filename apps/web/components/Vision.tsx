6fc04e6c3a53d14106c5fc031f943a7deceb875c
sakshi <ayushmanj138@gmail.com>
2024-06-29 16:57:05 +0530

    feat: vision page

--- apps/web/components/Vision.tsx
+++ apps/web/components/Vision.tsx
@@ -2,29 +2,29 @@ import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from
 
 const features = [
     {
-        name: 'Push to deploy',
+        name: 'Inclusive Learning',
         description:
             'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
         icon: CloudArrowUpIcon,
     },
     {
-        name: 'SSL certificates',
+        name: 'Enhance the reading Experience',
         description:
             'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
         icon: LockClosedIcon,
     },
     {
-        name: 'Simple queues',
+        name: 'Empower Learning',
         description:
             'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
         icon: ArrowPathIcon,
     },
-    {
-        name: 'Advanced security',
-        description:
-            'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-        icon: FingerPrintIcon,
-    },
+    // {
+    //     name: 'Advanced security',
+    //     description:
+    //         'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+    //     icon: FingerPrintIcon,
+    // },
 ];
 
 export default function Vision() {
@@ -32,9 +32,9 @@ export default function Vision() {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+                    {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2> */}
                     <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Everything you need to deploy your app
+                        Our vision
                     </p>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
                         Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
