dab3ac64e16360e554b0bf0207329e73d1ea6ca6
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-06-29 19:59:40 +0530

    remove logo

--- apps/web/components/Hero.tsx
+++ apps/web/components/Hero.tsx
@@ -227,11 +227,11 @@ export default function Hero() {
                                 <div className="flex lg:flex-1">
                                     <a href="#" className="-m-1.5 p-1.5">
                                         <span className="sr-only">Your Company</span>
-                                        <img
+                                        {/* <img
                                             className="h-8 w-auto"
                                             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                             alt=""
-                                        />
+                                        /> */}
                                     </a>
                                 </div>
                                 <div className="hidden lg:flex lg:gap-x-12 lg:justify-center">
