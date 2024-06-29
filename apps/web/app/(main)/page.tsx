a1757ad02d47c260aa41e202bd966b82b35f275e
Puskar-Roy <puskarroy600@gmail.com>
2024-06-29 11:04:55 +0530

    feat : protect

--- apps/web/app/(main)/page.tsx
+++ apps/web/app/(main)/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import React, { useEffect } from "react";
-import Contact from "../components/Contact";
-import Features from "../components/Features";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import Vision from "../components/Vision";
+import Contact from "../../components/Contact";
+import Features from "../../components/Features";
+import Footer from "../../components/Footer";
+import Hero from "../../components/Hero";
+import Navbar from "../../components/Navbar";
+import Vision from "../../components/Vision";
 
 export default function Home() {
   useEffect(() => {
-
     document.documentElement.style.scrollBehavior = "smooth";
   }, []);
+
 
   return (
     <div className="flex flex-col min-h-screen w-full">
